cb04d321beb2a4badd0a5534c539456b9885a0c5
Piyush <piyushagal@salesforce.com>
2016-06-22 15:11:09 -0700

    Period Works Now

--- testHistoryServer.js
+++ testHistoryServer.js
@@ -3,46 +3,93 @@ var http = require('http'),
     fs = require('fs'),
     mongodb = require("mongodb");;
 //Lets define a port we want to listen to
-const PORT=8080; 
+const PORT = 8080;
 var dbHost = "mongodb://localhost:27017/testHistory";
 var dbObject;
 var MongoClient = mongodb.MongoClient;
-MongoClient.connect(dbHost, function(err, db){
-  if ( err ) throw err;
-  dbObject = db;
+MongoClient.connect(dbHost, function(err, db) {
+    if (err) throw err;
+    dbObject = db;
 });
 
 //We need a function which handles requests and send response
-function handleRequest(request, response){
+function handleRequest(request, response) {
     var path = url.parse(request.url).pathname;
     var query = url.parse(request.url, true).query;
-    
-    //response.end('It Works!! Path Hit: ' + JSON.stringify(query));
-        fs.readFile('ftest.html', 'utf-8', function (err, data) {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        var chartData = [];
-        for (var i = 0; i < 7; i++)
-            chartData.push(Math.random() * 50);
-            
-        var result = data.replace('{{chartData}}', JSON.stringify(chartData));
-            if(path == "/ftest.html"){
-                getData(query.testName);
-                result = result.replace('Working Chart', query.testName);
-            }
-	response.write(result);
-	response.end();
-	});
-}
 
-function getData(testName){
-    dbObject.collection("testH").find({"TEST" :  testName}).toArray(
-    function(err, docs){
-        if ( err ) throw err;
-        for(index in docs){
-            var doc = docs[index];
-            console.log(doc['DATE'] + " : " + doc['STATUS']);
+    //response.end('It Works!! Path Hit: ' + JSON.stringify(query));
+    fs.readFile('ftest.html', 'utf-8', function(err, data) {
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        var result = data.replace('Working Chart', query.testName);
+        if (path == "/ftest.html") {
+            if(query.period == undefined){
+            dbObject.collection("test").find({
+                "TEST": query.testName
+            }).sort({'DATE' : -1}).toArray(
+                function(err, docs) {
+                    var color = [];
+                    var runTime = [];
+                    var labels = [];
+                    if (err) throw err;
+                    for (index in docs) {
+                        var doc = docs[index];
+                        runTime.push(parseInt(doc['RUNTIME']));
+                        labels.push(doc['DATE']);
+                        color.push(doc['STATUS']);
+                    }
+                    //console.log(JSON.stringify(runTime));
+                    result = result.replace('{{labels}}', JSON.stringify(labels));
+                    result = result.replace('{{chartData}}', JSON.stringify(runTime));
+                    result = result.replace('{{status}}', JSON.stringify(color));
+                    response.write(result);
+                    response.end();
+                });
+        }else{
+            var today = new Date();
+            var todayString = (today.getMonth()+1) + "/" + (today.getDate().toString()) + "/" + today.getFullYear().toString();
+            var prevDate = new Date();
+            if(query.period == 'LastWeek'){
+                prevDate.setDate(today.getDate() - 7);
+            }else if(query.period == 'LastMonth'){
+                prevDate.setDate(today.getDate() - 30);
+            }else if(query.period == 'LastQuarter'){
+                prevDate.setDate(today.getDate() - 90);
+            }
+            
+            var prevDateString = (prevDate.getMonth()+1) + "/" + (prevDate.getDate().toString()) + "/" + prevDate.getFullYear().toString();
+            
+            console.log(todayString);
+            dbObject.collection("test").find({
+                "TEST": query.testName, "DATE" : {$gt : prevDateString, $lt : todayString}
+            }).sort({'DATE' : -1}).toArray(
+                function(err, docs) {
+                    var color = [];
+                    var runTime = [];
+                    var labels = [];
+                    if (err) throw err;
+                    for (index in docs) {
+                        var doc = docs[index];
+                        runTime.push(parseInt(doc['RUNTIME']));
+                        labels.push(doc['DATE']);
+                        color.push(doc['STATUS']);
+                    }
+                    //console.log(JSON.stringify(runTime));
+                    result = result.replace('{{labels}}', JSON.stringify(labels));
+                    result = result.replace('{{chartData}}', JSON.stringify(runTime));
+                    result = result.replace('{{status}}', JSON.stringify(color));
+                    response.write(result);
+                    response.end();
+                });
+        }
         }
     });
+}
+
+function getData(testName) {
+    var color = [];
+    var runTime = [];
 
 }
 
@@ -50,7 +97,7 @@ function getData(testName){
 var server = http.createServer(handleRequest);
 
 //Lets start our server
-server.listen(PORT, function(){
+server.listen(PORT, function() {
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
