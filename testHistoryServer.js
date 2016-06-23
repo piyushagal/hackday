8f06423ff2477c03df1e2079c05998903c98b0ae
Piyush <piyushagal@salesforce.com>
2016-06-22 19:16:09 -0700

    Pie&Autobuild added

--- testHistoryServer.js
+++ testHistoryServer.js
@@ -12,60 +12,51 @@ MongoClient.connect(dbHost, function(err, db) {
     dbObject = db;
 });
 
+var autobuild = "";
+
 //We need a function which handles requests and send response
 function handleRequest(request, response) {
     var path = url.parse(request.url).pathname;
     var query = url.parse(request.url, true).query;
+    var statusIn = ["y", "e"];
 
-    //response.end('It Works!! Path Hit: ' + JSON.stringify(query));
-    fs.readFile('ftest.html', 'utf-8', function(err, data) {
-        response.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        var result = data.replace('Working Chart', query.testName);
         if (path == "/ftest.html") {
-            if(query.period == undefined){
-            dbObject.collection("test").find({
-                "TEST": query.testName
-            }).sort({'DATE' : -1}).toArray(
-                function(err, docs) {
-                    var color = [];
-                    var runTime = [];
-                    var labels = [];
-                    if (err) throw err;
-                    for (index in docs) {
-                        var doc = docs[index];
-                        runTime.push(parseInt(doc['RUNTIME']));
-                        labels.push(doc['DATE']);
-                        color.push(doc['STATUS']);
-                    }
-                    //console.log(JSON.stringify(runTime));
-                    result = result.replace('{{labels}}', JSON.stringify(labels));
-                    result = result.replace('{{chartData}}', JSON.stringify(runTime));
-                    result = result.replace('{{status}}', JSON.stringify(color));
-                    response.write(result);
-                    response.end();
+            autobuild = "";
+            fs.readFile('ftest.html', 'utf-8', function(err, data) {
+                response.writeHead(200, {
+                'Content-Type': 'text/html'
                 });
-        }else{
+            var result = data.replace('Working Chart', query.testName);
+            if(query.status == 'Passed')
+                statusIn = ['y'];
+            else if(query.status == 'Failed')
+                statusIn = ['e'];
+                
             var today = new Date();
             var todayString = (today.getMonth()+1) + "/" + (today.getDate().toString()) + "/" + today.getFullYear().toString();
-            var prevDate = new Date();
-            if(query.period == 'LastWeek'){
-                prevDate.setDate(today.getDate() - 7);
-            }else if(query.period == 'LastMonth'){
-                prevDate.setDate(today.getDate() - 30);
-            }else if(query.period == 'LastQuarter'){
-                prevDate.setDate(today.getDate() - 90);
+            
+            var testNameQuery = {"TEST" : query.testName};
+            testNameQuery["STATUS"] = {$in : statusIn};
+            if(query.period!=undefined && query.period != 'All'){
+                var prevDate = new Date();
+                if(query.period == 'LastWeek'){
+                    prevDate.setDate(today.getDate() - 7);
+                }else if(query.period == 'LastMonth'){
+                    prevDate.setDate(today.getDate() - 30);
+                }else if(query.period == 'LastQuarter'){
+                    prevDate.setDate(today.getDate() - 90);
+                }
+
+                var prevDateString = (prevDate.getMonth()+1) + "/" + (prevDate.getDate().toString()) + "/" + prevDate.getFullYear().toString();
+                testNameQuery["DATE"]  = {$gt : prevDateString, $lte : todayString};
             }
-            
-            var prevDateString = (prevDate.getMonth()+1) + "/" + (prevDate.getDate().toString()) + "/" + prevDate.getFullYear().toString();
-            
-            console.log(todayString);
-            dbObject.collection("test").find({
-                "TEST": query.testName, "DATE" : {$gt : prevDateString, $lt : todayString}
-            }).sort({'DATE' : -1}).toArray(
+            if(query.autobuild != undefined){
+                testNameQuery["AUTOBUILD"] = query.autobuild;
+            }
+            debugger;
+            dbObject.collection("test").find(testNameQuery).sort({"DATE" : -1}).toArray(
                 function(err, docs) {
-                    var color = [];
+                    var color = "";
                     var runTime = [];
                     var labels = [];
                     if (err) throw err;
@@ -73,18 +64,31 @@ function handleRequest(request, response) {
                         var doc = docs[index];
                         runTime.push(parseInt(doc['RUNTIME']));
                         labels.push(doc['DATE']);
-                        color.push(doc['STATUS']);
+                        color = color.concat(doc['STATUS']);
+                        if(autobuild.indexOf(doc['AUTOBUILD'])<0){
+                            autobuild = autobuild.concat("," + doc['AUTOBUILD']);
+                        }
                     }
                     //console.log(JSON.stringify(runTime));
                     result = result.replace('{{labels}}', JSON.stringify(labels));
                     result = result.replace('{{chartData}}', JSON.stringify(runTime));
-                    result = result.replace('{{status}}', JSON.stringify(color));
+                    result = result.replace('{{status}}', color);
+                    if(query.period!=undefined){
+                        result = result.replace('<option value="' + query.period + '"', '<option    value="' + query.period + '" selected');
+                    }
                     response.write(result);
                     response.end();
                 });
+
+          });
+        }else if(path == "/autobuild"){
+            response.writeHead(200, {
+                'Content-Type': 'text'
+            });
+            console.log("Autobuild = " + autobuild);
+            response.write(autobuild);
+            response.end();
         }
-        }
-    });
 }
 
 function getData(testName) {
